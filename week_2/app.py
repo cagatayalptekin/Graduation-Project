2a9e6cf8b31398398671d6ebe84102be6882276a
ali <senyor.19011@gmail.com>
2024-03-10 01:40:04 +0300

    Add codes for Llama2 model

--- week_2/app.py
+++ week_2/app.py
@@ -1,2 +1,22 @@
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import CTransformers
 
-print("deneme")
+llm = CTransformers(model='models/llama-2-7b-chat.ggmlv3.q4_0.bin',     # Save the models you will use in the modles file. 
+                    model_type='llama',                                 # link to Llama2 quantized model: https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/tree/main
+                    config={'max_new_tokens': 256,
+                            'temperature': 0.01})
+
+
+template = """# Description: {text}
+# Task: Write Python code that accomplishes the above task."""
+
+text = "Code that adds numbers from 1 to n."
+text_1="Function that calculates n'th Fibonacci numbers recursively"
+
+prompt = PromptTemplate(template=template, input_variables=["text"])
+
+prompt_text = template.format(text=text_1)
+
+
+response=llm(prompt_text)
+print(response)
